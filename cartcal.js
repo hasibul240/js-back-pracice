e397833c56af2d787b178bc16b49968e3eac4ba7
hasibul240 <hasibul112002@gmail.com>
2022-08-10 16:40:38 +0600

    quentity

--- cartcal.js
+++ cartcal.js
@@ -1,14 +1,15 @@
 const shopingcart = [
-    { name: 'shoe', price: '1200' },
-    { name: 'shirt', price: '2200' },
-    { name: 'pant', price: '3700' },
-    { name: 'balt', price: '600' },
+    { name: 'shoe', price: '1200', quentity:'2' },
+    { name: 'shirt', price: '2200', quentity:'4' },
+    { name: 'pant', price: '3700', quentity:'6' },
+    { name: 'balt', price: '600', quentity:'3' },
 ];
-// Calculate the total cost of the products in a shopping cart
-function totalCost(cart) {
+// Calculate the total cost of the products in a shopping
+function totalCost(shopingcart) {
     let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-        total += Number(cart[i].price);
+    for (let i = 0; i < shopingcart.length; i++) {
+        const product = shopingcart[i];
+        total = total + (product.price * product.quentity);
     }
     return total;
 }
